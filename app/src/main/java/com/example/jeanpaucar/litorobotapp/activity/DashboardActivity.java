bd4a95caa42e862bd68f255cd7c77bf3b9bbac10
Jeancarlos <jpaucarm6@gmail.com>
2016-09-20 23:59:44 -0500

    Dashboard - fragent

--- app/src/main/java/com/example/jeanpaucar/litorobotapp/activity/DashboardActivity.java
+++ app/src/main/java/com/example/jeanpaucar/litorobotapp/activity/DashboardActivity.java
@@ -4,14 +4,21 @@ import android.app.Activity;
 import android.os.Bundle;
 
 import com.example.jeanpaucar.litorobotapp.R;
+import com.example.jeanpaucar.litorobotapp.fragment.DashboardFragment;
 
 /**
  * Created by jose.paucar on 20/09/2016.
  */
 public class DashboardActivity extends Activity{
+
+    private DashboardFragment dashboardFragment;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_content);
+
+        dashboardFragment = new DashboardFragment();
+        getFragmentManager().beginTransaction().replace(R.id.act_main_fragment_container, dashboardFragment, null).commit();
     }
 }
